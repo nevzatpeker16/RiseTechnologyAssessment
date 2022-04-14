0dcf9b47ede14861eb22cfee8393873d938f9c56
nevzatpeker16 <77233554+nevzatpeker16@users.noreply.github.com>
2022-04-14 15:29:35 +0300

    geliştirme.

--- DataAccess/Concrete/EntityFramework/EFPersonDal.cs
+++ DataAccess/Concrete/EntityFramework/EFPersonDal.cs
@@ -18,7 +18,23 @@ namespace DataAccess.Concrete.EntityFramework
 
         public List<PersonInformationDetail> GetPersonInformationDetails(Person person)
         {
-            return null;
+            using (MyDbContext dbContext = new MyDbContext())
+            {
+                var result = from pe in dbContext.Persons
+                             join c in dbContext.Companies on pe.CompanyID equals c.CompanyID
+                             join pı in dbContext.PersonInformation on pe.ID equals pı.PersonID
+                             where pe.ID == person.ID
+                             select new PersonInformationDetail
+                             {
+                                 PersonID = pe.ID,
+                                 PersonName = pe.Name,
+                                 PersonSurname = pe.Surname,
+                                 PersonCompany = c.Company,
+                                 ContactInformations = pı.ContactInformations,
+                             };
+
+                return result;
+            }
         }
     }
 }
--- DataAccess/Concrete/EntityFramework/MyDbContext.cs
+++ DataAccess/Concrete/EntityFramework/MyDbContext.cs
@@ -9,7 +9,7 @@ namespace DataAccess.Concrete.EntityFramework
             => optionsBuilder.UseNpgsql("Host=localhost;Database=postgres;Username=postgres;Password=Fatma_16");
 
         public DbSet<Company> Companies { get; set; }
-        public DbSet<PersonInformation> ContactInformations { get; set; }
+        public DbSet<PersonInformation> PersonInformation { get; set; }
         public DbSet<InformationType> InformationTypes { get; set; }
         public DbSet<Person> Persons { get; set; }
 
